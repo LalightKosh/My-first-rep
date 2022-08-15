06bd3cd0e3217ef02b424b15b2c87ef64beb9450
totfront <totfront@gmail.com>
2022-08-15 19:32:29 +0200

    feat: restructed the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <img
+          className="header__logo"
+          src="/src/images/logo_place_header.svg"
+          width="228px"
+          height="32px"
+          alt="logo"
+        />
+        <div classnames="title">Научиться учиться</div>
+        <div classnames="subtitle">
+          Какие современные и эффективные подходы к обучению вы можете
+          использовать в своей жизни?
+        </div>
+        {/* <div class = "link">Узнать</div> */}
+        <div class="main-illustration">
+          <img
+            src="./src/images/two_again.png"
+            height="608px"
+            width="765px"
+            alt="Family_picture"
+          />
+        </div>
+        <div class="header__square-pic"></div>
       </header>
+
+      <div class="content">
+        <div class="description">
+          <div class="two-columns">
+            <div class="brief">Главные проблемы в обучении</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
