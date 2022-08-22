c4da770251a356d7591d08b317e89faeb9938510
Roman Kosh <110398466+LalightKosh@users.noreply.github.com>
2022-08-22 21:00:53 +0400

    chore: fixes around reviewers comments

--- src/App.js
+++ src/App.js
@@ -16,8 +16,10 @@ const App = () => {
         <h1 className="header__title">Научиться учиться</h1>
         <p className="header__subtitle">
           Какие современные и эффективные подходы к обучению вы можете
-          использовать в своей жизни?&nbsp;
-          <a href="https://en.wikipedia.org/wiki/Wiki">Узнать</a>
+          использовать в своей жизни?
+          <a href="https://en.wikipedia.org/wiki/Wiki" className="header__link">
+            Узнать
+          </a>
         </p>
         <img
           className="header__image"
@@ -29,22 +31,22 @@ const App = () => {
         <div class="header__square-pic" />
       </header>
 
-      <section className="content__description">
+      <section className="description">
         <div className="description__two-columns">
-          <h3 className="two-columns__brief">Главные проблемы в обучении</h3>
-          <div className="two-columns__main-text">
-            <p className="description__p">
+          <h3 className="description__header">Главные проблемы в обучении</h3>
+          <div className="description__main-text">
+            <p className="description__text">
               Ни в школе, ни в институте нас не учат тому, как правильно изучать
               материал. Мы готовимся к экзаменам и учим билеты. Мы тренируемся
               решать однообразные задачи, чтобы лучше сдать тест, но часто в
               итоге это не дает нам реального знания. Зубрежка быстро
               выветривается и не приносит пользы.
             </p>
-            <p className="description__p">
+            <p className="description__text">
               <span className="span-accent">Вывод:</span> учиться тоже нужно
               уметь, но почему-то этому мало где учат. Что с этим делать?
             </p>
-            <p className="description__p">
+            <p className="description__text">
               Конкретные техники и упражнения помогают изменить подход к
               обучению, сделать его эффективным и захватывающим. Эти же техники
               применяются на примере обучения в Практикуме.
